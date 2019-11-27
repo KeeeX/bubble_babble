a2e954f68afbf8abc7fb30e6a55fcd8afed8681f
Gabriel Paul 'Cley Faye' Risterucci <gabriel@keeex.net>
2019-11-27 15:27:35 +0100

    Formatting

--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
 /*eslint-env node, mocha */
-const bubble = require("../bubble_babble");
-require("should");
+import "should";
+import bubble from "..";
 
-var test_vectors = [
+const random_int = (min, max) => Math.floor(
+  Math.random() * (max - min + 1) + min
+);
+
+const random_str = () => Math.random().toString(36).substr(
+  2,
+  random_int(0, 11)
+);
+
+const test_vectors = [
   {
     ascii: "",
     encoding: "xexax"
@@ -17,47 +26,37 @@ var test_vectors = [
   }
 ];
 
-var random_int = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+describe("BubbleBabble", () => {
+  describe("#encode()", () => {
+    it("should encode a buffer", () =>
+      test_vectors.forEach(test =>
+        bubble.encode(Buffer.from(test.ascii)).should.equal(test.encoding)
+      )
+    );
 
-var random_str = function() {
-  return Math.random().toString(36).substr(2, random_int(0, 11));
-};
+    it("should encode a string", () =>
+      test_vectors.forEach(test =>
+        bubble.encode(test.ascii, "ascii").should.equal(test.encoding)
+      )
+    );
 
-describe("BubbleBabble", function() {
-  describe("#encode()", function() {
-    it("should encode a buffer", function() {
-      test_vectors.forEach(function(test) {
-        bubble.encode(Buffer.from(test.ascii)).should.equal(test.encoding);
-      });
-    });
-
-    it("should encode a string", function() {
-      test_vectors.forEach(function(test) {
-        bubble.encode(test.ascii, "ascii").should.equal(test.encoding);
-      });
-    });
-
-    it("should return a string that starts and begins with \"x\"", function() {
-      var encoded;
-
+    it("should return a string that starts and begins with \"x\"", () => {
       for (let i = 0; i < 10; ++i) {
-        encoded = bubble.encode(random_str());
+        const encoded = bubble.encode(random_str());
         encoded.should.startWith("x");
         encoded.should.endWith("x");
       }
     });
 
-    it("should be the inverse of decoding", function() {
-      var encoding = "xesef-disof-gytuf-katof-movif-baxux";
+    it("should be the inverse of decoding", () => {
+      const encoding = "xesef-disof-gytuf-katof-movif-baxux";
       bubble.encode(bubble.decode(encoding)).should.equal(encoding);
     });
   });
 
-  describe("#decode()", function() {
-    it("should decode a string and return a buffer", function() {
-      test_vectors.forEach(function(test) {
+  describe("#decode()", () => {
+    it("should decode a string and return a buffer", () => {
+      test_vectors.forEach(test => {
         var decoded = bubble.decode(test.encoding);
         Buffer.isBuffer(decoded).should.be.true;
 
@@ -65,29 +64,24 @@ describe("BubbleBabble", function() {
       });
     });
 
-    it("should throw exception on corrupt input", function() {
-      (function() {
-        bubble.decode("xesyf-disof-gytuf-katof-movif-baxux");
-      }).should.throw;
+    it("should throw exception on corrupt input", () => {
+      (() => bubble.decode("xesyf-disof-gytuf-katof-movif-baxux")).should.throw;
 
-      (function() {
-        bubble.decode("xesef-disof-gytuf-katof-movif-baxu");
-      }).should.throw;
+      (() => bubble.decode("xesef-disof-gytuf-katof-movif-baxu")).should.throw;
     });
 
-    it("should be inverse of encoding a string", function() {
-      var ascii_input = "Inverse of each other.";
+    it("should be inverse of encoding a string", () => {
+      const ascii_input = "Inverse of each other.";
 
       bubble.decode(
         bubble.encode(ascii_input)
       ).toString().should.equal(ascii_input);
     });
 
-    it("should be inverse of encoding a buffer", function() {
-      var input = Buffer.from("ffffffff","hex");
+    it("should be inverse of encoding a buffer", () => {
+      const input = Buffer.from("ffffffff","hex");
 
       bubble.decode(bubble.encode(input)).equals(input).should.be.true;
     });
   });
-
 });
